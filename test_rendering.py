2c1764005aac925da6cc4003d16fa9959390555a
emma <emma.ee13@nycu.edu.tw>
2025-06-17 11:52:41 +0800

    test_code

--- test_rendering.py
+++ test_rendering.py
@@ -10,23 +10,23 @@ class TestUIRendering(unittest.TestCase):
         self.ui.set_game_state_reference(self.mock_game_state)
 
     @patch('pygame.draw.rect')
-    def test_sanity_bar_rendering(self, mock_draw):
-        """測試精神值條渲染"""
+    def test_infection_bar_rendering(self, mock_draw):
+        """测试感染条渲染"""
         self.mock_game_state.player_stats = {
-            "sanity": 60,
-            "max_sanity": 100
+            "infection_level": 75,
+            "max_infection": 100
         }
         
         self.ui.render_hud(self.mock_game_state, None)
         
-        # 驗證精神值條繪製
-        args, _ = mock_draw.call_args_list[2]  # 第三個繪製條
-        self.assertAlmostEqual(args[0].width, 200 * (60/100), delta=1)
+        # 验证感染条绘制
+        args, _ = mock_draw.call_args_list[3]  # 第四个绘制条
+        self.assertAlmostEqual(args[0].width, 200 * (75/100), delta=1)
 
     @patch('pygame.Surface')
-    def test_weather_overlay(self, mock_surface):
-        """測試天氣覆蓋層渲染"""
-        self.mock_game_state.weather = "sandstorm"
+    def test_blizzard_overlay(self, mock_surface):
+        """测试暴风雪覆盖层渲染"""
+        self.mock_game_state.environment = {"weather": "blizzard"}
         self.ui.render(self.mock_game_state, None, None)
         mock_surface.assert_called_with((self.ui.screen_width, self.ui.screen_height), pygame.SRCALPHA)
 
